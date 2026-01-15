77c72d739b5b4b64c1d47b7796f75cd2dc1c06cd
João Pedro Marques <joaopedrobmb@gmail.com>
2026-01-15 16:13:35 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Teste de deploy no vercel</h1>
+  return <h1>Teste de deploy no vercel</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function testes() {
+  console.log("identação errada");
 }
 
 export default Home;
