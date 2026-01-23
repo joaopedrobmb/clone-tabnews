64737cbb908a7d6e0f09029cc06f18c88680793b
joaopedrobmb <joaopedrobmb@gmail.com>
2026-01-23 15:13:44 -0300

    adds `/api/v1/migrations` endpoint.

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config({
+  path: ".env.development",
+});
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "." });
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
+
+// provar através do código que o processo do jest ta rodando no ambiente test
+// conseguir carregar no processo do jest epro database.js conseguir se conectar ao
