e41619559476bf69a1841a266a92dbbe490854e6
joaopedrobmb <joaopedrobmb@gmail.com>
2026-01-29 11:29:03 -0300

    add `async-retry` dev-dependency

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,3 @@ const jestConfig = createJestConfig({
 });
 
 module.exports = jestConfig;
-
-// provar através do código que o processo do jest ta rodando no ambiente test
-// conseguir carregar no processo do jest epro database.js conseguir se conectar ao
