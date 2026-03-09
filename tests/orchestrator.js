937cd59634cc645cb13163f7864f353ba7e19723
joaopedrobmb <joaopedrobmb@gmail.com>
2026-03-09 10:31:54 -0300

    feat: create `authorization` model and use it inside `/sessions` controller

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,6 +5,7 @@ import database from "infra/database";
 import migrator from "models/migrator.js";
 import user from "models/user.js";
 import session from "models/session";
+import activation from "models/activation";
 
 const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
 
@@ -85,6 +86,10 @@ function extractUUID(text) {
   return match ? match[0] : null;
 }
 
+async function activateUser(inactiveUser) {
+  return await activation.activateUserByUserId(inactiveUser.id);
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
@@ -94,6 +99,7 @@ const orchestrator = {
   deleteAllEmail,
   getLastEmail,
   extractUUID,
+  activateUser,
 };
 
 export default orchestrator;
