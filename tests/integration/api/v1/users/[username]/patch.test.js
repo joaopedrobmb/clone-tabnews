d9207d81e26745977f7bf98e614e742c4df3572f
joaopedrobmb <joaopedrobmb@gmail.com>
2026-03-10 11:27:32 -0300

    feat: allow `update:user:others` to update other users

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -10,32 +10,33 @@ beforeAll(async () => {
 });
 
 describe("PATCH /api/v1/users/[username]", () => {
-  describe("Anonymous user", () => {});
-  test("With unique 'username'", async () => {
-    const createdUser = await orchestrator.createUser({});
+  describe("Anonymous user", () => {
+    test("With unique 'username'", async () => {
+      const createdUser = await orchestrator.createUser({});
 
-    const response = await fetch(
-      `http://localhost:3000/api/v1/users/${createdUser.username}`,
-      {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
+      const response = await fetch(
+        `http://localhost:3000/api/v1/users/${createdUser.username}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "uniqueuser2",
+          }),
         },
-        body: JSON.stringify({
-          username: "uniqueuser2",
-        }),
-      },
-    );
+      );
 
-    expect(response.status).toBe(403);
+      expect(response.status).toBe(403);
 
-    const responseBody = await response.json();
+      const responseBody = await response.json();
 
-    expect(responseBody).toEqual({
-      action: 'Verifique se o seu usuário possui a feature "update:user',
-      message: "Você não possui permissão para executar esta ação.",
-      name: "ForbiddenError",
-      status_code: 403,
+      expect(responseBody).toEqual({
+        action: 'Verifique se o seu usuário possui a feature "update:user',
+        message: "Você não possui permissão para executar esta ação.",
+        name: "ForbiddenError",
+        status_code: 403,
+      });
     });
   });
   describe("Default user", () => {
@@ -312,6 +313,57 @@ describe("PATCH /api/v1/users/[username]", () => {
         action: "Utilize outro email para realizar esta operação.",
         status_code: 400,
       });
+    });
+  });
+  describe("Privileged user", () => {
+    test("With `update:user:other` targeting `defaultUser`", async () => {
+      const privilegedUser = await orchestrator.createUser({});
+      const activatedPrivilegedUser =
+        await orchestrator.activateUser(privilegedUser);
+
+      await orchestrator.addFeaturesToUser(privilegedUser, [
+        "update:user:others",
+      ]);
+
+      const privilegedUserSession = await orchestrator.createSession(
+        activatedPrivilegedUser.id,
+      );
+
+      const defaultUser = await orchestrator.createUser({});
+
+      const response = await fetch(
+        `http://localhost:3000/api/v1/users/${defaultUser.username}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Cookie: `session_id=${privilegedUserSession.token}`,
+          },
+          body: JSON.stringify({
+            username: "AlteradoPorPrivilegiado",
+          }),
+        },
+      );
+
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: defaultUser.id,
+        username: "AlteradoPorPrivilegiado",
+        email: defaultUser.email,
+        password: responseBody.password,
+        features: defaultUser.features,
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      expect(responseBody.updated_at > responseBody.created_at).toBe(true);
     });
   });
 });
